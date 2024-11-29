1eddb8bb354dbe9c9bc97a625ec9c6145dd13032
CristinaR08 <cris.04082009@hotmail.com>
2024-11-29 00:40:47 +0000

    Prueba unidad 1

---
+++ js/logica.js
@@ -0,0 +1,12 @@
+const guardado = (cedula,nombre,apellido) =>{
+    json(cedula,nombre,apellido);
+    console(json)
+}
+
+const json = {
+    cedula: document.getElementById("cedula"),
+    nombre: document.getElementById("nombre"),
+    apellido: document.getElementById("apellido"),
+    
+}
+
